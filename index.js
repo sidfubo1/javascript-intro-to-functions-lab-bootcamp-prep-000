9c6b00ff208a246aed5e9b742b660167a6ba06d2
sidfubo1 <sidfubo1@gmail.com>
2018-09-10 13:53:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,3 +7,9 @@ function whisper (string) {
   string = string.toLowerCase();
   return string;
 }
+
+function logShout (string) {
+  string = string.toUpperCase();
+  return string;
+  console.log(string);
+}
